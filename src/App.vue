0e785e0137dba281e09841a42de57b95726bf542
Alberto Polo <bertopolo91@gmail.com>
2024-02-20 20:25:30 +0100

    adding premade css

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@ import Header from "./components/Header.vue"
 import Balance from "./components/Balance.vue"
 import IncomeExpenses from "./components/IncomeExpenses.vue"
 import TransactionList from "./components/TransactionList.vue"
+import AddTransaction from "./components/AddTransaction.vue"
 
 export default {
-  components: { Header, Balance, IncomeExpenses, TransactionList },
+  components: { Header, Balance, IncomeExpenses, TransactionList, AddTransaction },
 }
 </script>
 
@@ -15,5 +16,6 @@ export default {
     <Balance />
     <IncomeExpenses />
     <TransactionList />
+    <AddTransaction />
   </div>
 </template>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,3 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: "Flower", amount: -19.99 },
+        { id: 2, text: "Salary", amount: 9.1 },
+        { id: 3, text: "Book", amount: -19 },
+        { id: 4, text: "Camera", amount: 99 },
+      ],
+    }
+  },
+}
+</script>
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
