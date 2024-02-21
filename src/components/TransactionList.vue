9e1cb3f5802df5708c67caf861f090b1e75048ec
Alberto Polo <bertopolo91@gmail.com>
2024-02-21 16:02:50 +0100

    dynamic transaction list with composition

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,27 +1,26 @@
-<script>
-export default {
-  data() {
-    return {
-      transactions: [
-        { id: 1, text: "Flower", amount: -19.99 },
-        { id: 2, text: "Salary", amount: 9.1 },
-        { id: 3, text: "Book", amount: -19 },
-        { id: 4, text: "Camera", amount: 99 },
-      ],
-    }
-  },
-}
+<script setup>
+const transactions = [
+  { id: 1, text: "Flower", amount: -19.99 },
+  { id: 2, text: "Salary", amount: 9.1 },
+  { id: 3, text: "Book", amount: -19 },
+  { id: 4, text: "Camera", amount: 99 },
+]
 </script>
+
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">
+    <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount > 0 ? 'plus' : 'minus'">
+      {{ transaction.text }} <span>${{ transaction.amount }}</span>
+      <button class="delete-btn">x</button>
+    </li>
+    <!-- <li class="minus">
       Cash <span>$400</span>
       <button class="delete-btn">x</button>
     </li>
     <li class="plus">
       Paycheck <span>$800</span>
       <button class="delete-btn">x</button>
-    </li>
+    </li> -->
   </ul>
 </template>
